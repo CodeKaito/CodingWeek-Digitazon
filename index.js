4a421c9397f6ed2a22caa1da89195757f2a701a0
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-16 22:03:49 +0200

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+function checkAge(age) {
+    console.log(age >= 18)
+    if (age >= 18) {
+        console.log("sei maggio");
+    } else {
+        console.log("sei mino");
+    }
+}
+
+checkAge("a")
